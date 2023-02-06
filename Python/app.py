a91ca52f8c5d4d12b5e6151114a9c34a4e3116f6
Stephen O <stephenconochie@gmail.com>
2023-02-05 20:04:32 -0500

    Python Learning/Practice

--- Python/app.py
+++ Python/app.py
@@ -1,4 +1,9 @@
-price = 10
-rating = 4.9
-name 
-print(price)
+import math
+
+x = 2.499
+print(abs(x))
+print(math.ceil(2.9))
+
+#ended Python Tutorial at 57:25
+#Python Tutorial - Python Full Course for Beginners
+#Programming with Mosh
