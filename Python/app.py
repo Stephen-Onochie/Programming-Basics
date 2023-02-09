c0c04c95bee7f53d38843b662e782396b2e70fbc
Stephen-Onochie <stephenconochie@gmail.com>
2023-02-09 16:06:20 -0500

    Python Practice

--- Python/app.py
+++ Python/app.py
@@ -1,16 +1,24 @@
-is_hot = False
-is_cold = False
+# is_hot = False
+# is_cold = False
 
-if is_hot:
-    print("It's a hot day!")
-    print("Drink plenty of water")
-elif is_cold:
-    print("Its a cold day!")
-    print("Wear warm clothes!")
-else:
-    print("It's a lovely day!")
+# if is_hot:
+#     print("It's a hot day!")
+#     print("Drink plenty of water")
+# elif is_cold:
+#     print("Its a cold day!")
+#     print("Wear warm clothes!")
+# else:
+#     print("It's a lovely day!")
 
-print("Enjoy your day")
+# print("Enjoy your day")
+
+credit = "Good"
+
+if (credit == "Good"):
+    print("You need to put down 10%")
+elif credit == "Bad":
+    print("You need to put down 20&")
+print("This is based on your credit score")
 
 #ended Python Tutorial at 1:04:34
 #Python Tutorial - Python Full Course for Beginners
